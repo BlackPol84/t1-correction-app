2641f8e331160d52a473471cbd418e74df12af73
BlackPol84 <orlovpol84@gmail.com>
2024-11-09 10:08:10 +0300

    modified a ApplicationTests

--- src/test/java/ru/t1/correction/app/ApplicationTests.java
+++ src/test/java/ru/t1/correction/app/ApplicationTests.java
@@ -1,13 +1,8 @@
 package ru.t1.correction.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = "spring.application.scheduling.enable=false")
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
